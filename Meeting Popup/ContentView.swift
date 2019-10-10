774845529160b812f94527ce6c0f791f616437ff
Mike Crittenden <mikecrittenden@gmail.com>
2019-10-09 20:50:41 -0400

    Initial Commit

---
+++ Meeting Popup/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Meeting Popup
+//
+//  Created by Mike Crittenden on 10/9/19.
+//  Copyright © 2019 Mike Crittenden. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
